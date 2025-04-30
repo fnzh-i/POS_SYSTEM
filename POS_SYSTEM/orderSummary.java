e7d17bb15bfc98f209b3076654f0fa2215edd5a0
fnzh-i <fnzh.4u@gmail.com>
2025-05-01 03:54:51 +0800

    Update logs
    - removed sys.print
    
    + added notepad receipt method

--- POS_SYSTEM/orderSummary.java
+++ POS_SYSTEM/orderSummary.java
@@ -1,4 +1,8 @@
 package POS_SYSTEM;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class orderSummary {
    private double Total;
@@ -8,31 +12,36 @@ public class orderSummary {
    private double netTotal;
    private double totalVat;
 
+    public <doube> void orderSumm(doube cashTend, doube total){
+        // to follow pa yung order functions para sa receipt
 
-   public <doube> void orderSumm(doube cashTend, doube total){
-      // to follow pa yung order functions para sa receipt
+        Total = (double) total;
+        CashTend = (double) cashTend;
 
+        vatRate = 12;
+        netTotal = Total / (100 + vatRate) * 100;
+        totalVat = Total - netTotal;
+        change = CashTend - Total;
 
-      Total = (double) total;
-      CashTend = (double) cashTend;
+        BufferedWriter output;{
+            try {
+                output = new BufferedWriter(new FileWriter("Official_Receipt.txt"));
+                output.write("Total:       \t" + Total + "\n");
+                output.write("Cash Tendered:\t" + CashTend + "\n");
+                output.write("Change:      \t" + change + "\n");
+                output.write("=======================" + "\n");
+                output.write("VATable sales:\t");
+                output.write(String.format("%.5g",netTotal) + "\n");
+                output.write("VAT 12.0000%:\t");
+                output.write(String.format("%.4g",totalVat) + "\n");
+                output.write("=======================" + "\n");
+                output.close();
 
-      vatRate = 12;
-      netTotal = Total / (100 + vatRate) * 100;
-      totalVat = Total - netTotal;
-      change = CashTend - Total;
-
-      System.out.println("Total:       \t" + Total);
-      System.out.println("Cash Tendered:\t" + CashTend);
-      System.out.println("Change:      \t" + change);
-      System.out.println("=======================");
-      System.out.print("VATable sales:\t");
-      System.out.print(String.format("%.5g",netTotal) + "\n");
-      System.out.print("VAT 12.0000%:\t");
-      System.out.print(String.format("%.4g",totalVat) + "\n");
-      System.out.println("=======================");
-
-   }
-
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
 
 
